9e03ea94a0f322999a633624c71f3cd02d90c018
kno3comma14 <enyert.vinas@gmail.com>
2023-05-18 13:29:07 -0400

    More code cleaning

--- run-knowcomma.sh
+++ run-knowcomma.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # Execute the main class of the Java code
 LEINCP=`lein classpath`
-MVNCP=`mvn -q exec:exec -Dexec.executable=echo -Dexec.args="%classpath"`
 lein javac
 java -cp "$LEINCP" knowcomma.core.Knowcomma
